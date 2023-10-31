53038edad91e2b8d23a64ad17e6494c7305a3177
David <xkromkad@stuba.sk>
2023-10-31 23:42:04 +0100

    Add lab5 documentation

--- Lab 5/lab5_2.py
+++ Lab 5/lab5_2.py
@@ -1,23 +1,20 @@
 #author: David Kromka
-#Task 1
-print("Task 1")
-# List of hexadecimal elements
-hex_elements = ['01', '03', '04', '05', '06', '0d', '10']
+# Function to convert hexadecimal to decimal
+def hex_to_decimal(hex_string):
+    return int(hex_string, 16)
 
-# Convert hexadecimal to decimal and store in a list
-decimal_elements = [int(hex_element, 16) for hex_element in hex_elements]
+# Elements in direct replacement table
+direct_replacement_table = ["01", "03", "04", "05", "06", "0d", "10"]
 
-# Print the decimal elements
-print(decimal_elements)
+# Elements in reverse swap decryption table
+reverse_swap_table = ["f1", "f3", "f4", "f6", "f8", "ff", "10"]
 
-#Task 2
-print("Task 2")
-# List of hexadecimal elements
-hex_elements = ['f1', 'f3', 'f4', 'f6', 'f8', 'ff', '10']
+# Convert elements from hexadecimal to decimal
+direct_replacement_decimal = [hex_to_decimal(hex_string) for hex_string in direct_replacement_table]
+reverse_swap_decimal = [hex_to_decimal(hex_string) for hex_string in reverse_swap_table]
 
-# Convert hexadecimal to decimal and store in a list
-decimal_elements = [int(hex_element, 16) for hex_element in hex_elements]
+# Print the results
+print("Direct Replacement Table (Decimal Notation):", direct_replacement_decimal)
+print("Reverse Swap Decryption Table (Decimal Notation):", reverse_swap_decimal)
 
-# Print the decimal elements
-print(decimal_elements)
 
